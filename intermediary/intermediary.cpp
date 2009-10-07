d8086c260ade3d3af638684775735ebeb14e916b
void <git.email.address@googlemail.com>
2009-10-07 02:32:06 +0200

    I believe to have implemented functions... also got the operator precedence the wrong way, replaced < with >

--- intermediary/intermediary.cpp
+++ intermediary/intermediary.cpp
@@ -133,8 +133,8 @@ namespace frith
 		}
 
 		bool got_an_operator = false;
-		word minimum;
-		std::size_t minimum_offset;
+		word extremum;
+		std::size_t extremum_offset;
 
 		for(std::size_t i = 0, end = input.size(); i < end; i++)
 		{
@@ -145,13 +145,13 @@ namespace frith
 				if
 				(
 					!got_an_operator ||
-					precedence < minimum ||
-					(is_right_to_left_operator(current_node) && precedence == minimum)
+					precedence > extremum ||
+					(is_right_to_left_operator(current_node) && precedence == extremum)
 				)
 				{
 					got_an_operator = true;
-					minimum = precedence;
-					minimum_offset = i;
+					extremum = precedence;
+					extremum_offset = i;
 				}
 			}
 		}
@@ -159,30 +159,43 @@ namespace frith
 		if(!got_an_operator)
 			throw ail::exception("Failed to perform operator resolution");
 
-		parse_tree_node & operator_node = input[minimum_offset];
-		std::size_t next_offset = minimum_offset + 1;
-		if(operator_node.type == parse_tree_node_type::unary_operator_node)
+		parse_tree_node & operator_node = input[extremum_offset];
+		std::size_t next_offset = extremum_offset + 1;
+		switch(operator_node.type)
 		{
-			std::size_t argument_offset = next_offset;
-			parse_tree_unary_operator_node & unary_operator_node = *operator_node.unary_operator_pointer;
-			unary_operator_node.argument = input.at(argument_offset);
-			input.erase(input.begin() + argument_offset);
-		}
-		else
-		{
-			parse_tree_binary_operator_node & binary_operator_node = *operator_node.binary_operator_pointer;
+			case parse_tree_node_type::unary_operator_node:
+				std::size_t argument_offset = next_offset;
+				parse_tree_unary_operator_node & unary_operator_node = *operator_node.unary_operator_pointer;
+				unary_operator_node.argument = input.at(argument_offset);
+				input.erase(input.begin() + argument_offset);
+				break;
+		
+			case parse_tree_node_type::binary_operator_node:
+				parse_tree_binary_operator_node & binary_operator_node = *operator_node.binary_operator_pointer;
 
-			parse_tree_nodes
-				left_side,
-				right_side;
+				parse_tree_nodes
+					left_side,
+					right_side;
 
-			std::copy(input.begin(), input.begin() + minimum_offset, left_side.begin());
-			std::copy(input.begin() + next_offset, input.end(), right_side.begin());
+				std::copy(input.begin(), input.begin() + extremum_offset, left_side.begin());
+				std::copy(input.begin() + next_offset, input.end(), right_side.begin());
 
-			operator_resolution(left_side, binary_operator_node.left_argument);
-			operator_resolution(right_side, binary_operator_node.right_argument);
+				operator_resolution(left_side, binary_operator_node.left_argument);
+				operator_resolution(right_side, binary_operator_node.right_argument);
 
-			output = operator_node;
+				output = operator_node;
+				break;
+
+			case parse_tree_node_type::call:
+				//this is questionable
+				if(extremum_offset != 1)
+					throw ail::exception("Invalid call offset encountered during operator resolution");
+				operator_node.call_pointer->function = input[0];
+				input.erase(input.begin());
+				break;
+
+			default:
+				throw ail::exception("Invalid operator node type encountered during operator resolution");
 		}
 	}
 
@@ -238,7 +251,8 @@ namespace frith
 						parse_statement(lexemes, offset, content, true, lexeme_type::bracket_end);
 						parse_tree_node call;
 						call.is_call();
-						arguments.push_
+						call.call_pointer->arguments = content;
+						arguments.push_back(call);
 					}
 					else
 					{
